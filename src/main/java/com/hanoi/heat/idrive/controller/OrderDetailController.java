bf65ed93e4a65f89ff95eed3bf1f09ccc1cc9ca9
sangc1120g1 <sangdam202@gmail.com>
2021-06-19 21:39:03 +0700

    update order

--- src/main/java/com/hanoi/heat/idrive/controller/OrderDetailController.java
+++ src/main/java/com/hanoi/heat/idrive/controller/OrderDetailController.java
@@ -1,11 +1,25 @@
 package com.hanoi.heat.idrive.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hanoi.heat.idrive.model.OrderDetail;
+import com.hanoi.heat.idrive.service.OrderDetail.IOrderDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/Order")
 public class OrderDetailController {
+
+    @Autowired
+    private IOrderDetailService detailService;
+
+    @GetMapping("findByVehicle/{id}")
+    public ResponseEntity<?> findByVehicle(@PathVariable Long id){
+        return new ResponseEntity(detailService.findByVehicle(id), HttpStatus.OK);
+    }
+
 }
