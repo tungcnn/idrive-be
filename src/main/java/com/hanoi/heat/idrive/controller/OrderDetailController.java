105d515b413063733173376980720e832c10a6e2
sangc1120g1 <sangdam202@gmail.com>
2021-06-20 03:05:30 +0700

    Merge branch 'dev' of https://github.com/tungcnn/idrive-be into own_History
    # Conflicts:
    #	src/main/java/com/hanoi/heat/idrive/config/SecurityConfig.java

--- src/main/java/com/hanoi/heat/idrive/controller/OrderDetailController.java
+++ src/main/java/com/hanoi/heat/idrive/controller/OrderDetailController.java
@@ -1,19 +1,11 @@
 package com.hanoi.heat.idrive.controller;
 
-import com.hanoi.heat.idrive.model.OrderDetail;
 import com.hanoi.heat.idrive.service.OrderDetail.IOrderDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Date;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 
 @RestController
