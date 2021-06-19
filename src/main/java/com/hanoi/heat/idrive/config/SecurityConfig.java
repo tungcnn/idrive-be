9a4b1045e6e708f035dab3b8cf2691eab6c51af2
voduyquocthai <voduyquocthai@gmail.com>
2021-06-19 21:24:46 +0700

    commit

--- src/main/java/com/hanoi/heat/idrive/config/SecurityConfig.java
+++ src/main/java/com/hanoi/heat/idrive/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.hanoi.heat.idrive.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -19,5 +22,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers( "/review/**").hasAnyRole("USER")
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/hanoi/heat/idrive/controller/AuthController.java
+++ src/main/java/com/hanoi/heat/idrive/controller/AuthController.java
@@ -1,6 +1,24 @@
 package com.hanoi.heat.idrive.controller;
 
+import com.hanoi.heat.idrive.dto.RegisterRequest;
+import com.hanoi.heat.idrive.security.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+@CrossOrigin("*")
 public class AuthController {
 
+    private final AuthService authService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest){
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User Registration Successfully !", OK);
+    }
 }
